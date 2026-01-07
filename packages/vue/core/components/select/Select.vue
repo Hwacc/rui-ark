f37945c793bf9d1db9d76b2890b2633ce6ba779d
Hwacc <mshcccch@gmail.com>
2026-01-08 02:56:04 +0800

    feat: add Select component and integrate @zag-js/select for enhanced selection functionality
    - Introduced a new `Select` component in Vue with TypeScript support for better type safety.
    - Integrated `@zag-js/select` for improved selection handling and event management.
    - Updated `pnpm-lock.yaml` to include new dependencies and versions for `@zag-js` packages.
    - Enhanced TypeScript definitions for `SelectProps` and `SelectEmits` to ensure proper type inference.
    - Updated playground examples to demonstrate the new `Select` component and its integration with existing components.

---
+++ packages/vue/core/components/select/Select.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+export interface SelectProps<T extends CollectionItem> extends SelectRootProps<T> {
+  class?: HTMLAttributes['class']
+  unstyled?: boolean
+}
+// 这里我们必须重新定义SelectEmits类型, 否则Volar/VLS在推断ts时会报错ts-plugin(2742)
+// 这也导致必须将'@zag-js/select'包引入项目中, 所幸的是我们只需要其类型推断, 实际的runtime js依然会引用@ark-ui/vue中相关zag-js的runtime js
+export interface SelectEmits<T extends CollectionItem> {
+  'focusOutside': [event: select.FocusOutsideEvent]
+  'highlightChange': [details: select.HighlightChangeDetails<T>]
+  'interactOutside': [event: select.InteractOutsideEvent]
+  'openChange': [details: select.OpenChangeDetails]
+  'pointerDownOutside': [event: select.PointerDownOutsideEvent]
+  'select': [details: select.SelectionDetails]
+  'valueChange': [details: select.ValueChangeDetails<T>]
+  'update:modelValue': [value: string[]]
+  'update:open': [open: boolean]
+  'update:highlightedValue': [value: string | null]
+}
+</script>
+
+<script setup lang="ts" generic="T extends CollectionItem">
+import type { CollectionItem, SelectRootProps } from '@ark-ui/vue/select'
+import type * as select from '@zag-js/select'
+import type { HTMLAttributes } from 'vue'
+import { Select } from '@ark-ui/vue/select'
+import { useForwardPropsEmits } from '@ark-ui/vue/utils'
+import { useTheme } from '@rui-ark/vue-core/composables/useTheme'
+import { ThemeProvider } from '@rui-ark/vue-core/providers/theme'
+
+const { class: propsClass, unstyled, ...props } = defineProps<SelectProps<T>>()
+const emits = defineEmits<SelectEmits<T>>()
+const forwarded = useForwardPropsEmits(props, emits)
+
+const theme = useTheme({ size, unstyled })
+</script>
+
+<template>
+  <Select.Root v-bind="forwarded">
+    <ThemeProvider :value="theme">
+      <slot />
+    </ThemeProvider>
+  </Select.Root>
+</template>
