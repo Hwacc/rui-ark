021b4e8c082ed354d7e2d78e60a38a9118e287d8
Hwacc <mshcccch@gmail.com>
2026-02-03 11:39:36 +0800

    fix: correct directory name in Storybook configuration
    - Updated the Storybook configuration to change the directory name from 'add-ons' to 'addons' for consistency with the project's naming conventions.

--- packages/vue/.storybook/main.ts
+++ packages/vue/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from 'storybook-vue3-rsbuild'
 
 const config: StorybookConfig = {
-  stories: ['../core/**/*.stories.@(js|jsx|mjs|ts|tsx)', '../add-ons/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../core/**/*.stories.@(js|jsx|mjs|ts|tsx)', '../addons/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-docs', '@storybook/addon-a11y', '@storybook/addon-themes'],
   framework: {
     name: 'storybook-vue3-rsbuild',
