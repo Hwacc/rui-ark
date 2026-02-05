d669cd147739a7eefed56df775ab6b1ebeec80c6
Hwacc <mshcccch@gmail.com>
2026-02-05 11:00:42 +0800

    chore: update @ark-ui/vue dependency and enhance overlay stories
    - Updated @ark-ui/vue dependency to version 5.31.0-leading.1 in package.json.
    - Added raw code examples to the Overlay component stories for improved documentation.
    - Enhanced comments in the basic overlay example to clarify usage of the useOverlay composable.

--- packages/vue/core/components/overlay/examples/basic.vue
+++ packages/vue/core/components/overlay/examples/basic.vue
@@ -3,6 +3,11 @@ import { useOverlay } from '@rui-ark/vue/composables/useOverlay'
 import { Button } from '../../button'
 import BasicDialog from './components/BasicDialog.vue'
 
+/**
+ * UseOverlay with default options, will create a overlay list to manage the overlays
+ * Be careful when overlay close, the overlay object will still be in the list,
+ * its not be released totally
+ */
 const overlay = useOverlay()
 const basicDialog = overlay.create(BasicDialog)
 function handleOpenDialog() {
@@ -12,6 +17,10 @@ function handleOpenDialog() {
   })
 }
 
+/**
+ * UseOverlay with destroyOnClose option, it will will release the resource when overlay close
+ * but we shuold create a new one every time we open the dialog
+ */
 function handleOpenDestroyOnCloseDialog() {
   const destroyOnCloseDialog = overlay.create(BasicDialog, {
     destroyOnClose: true,
